074fbf165aaba338e76a68e82eb50d5e58c02f66
Indrek Pruul <ipruul@itcollege.ee>
2017-03-16 08:16:34 +0200

    js

--- 5/abakus.js
+++ 5/abakus.js
@@ -5,3 +5,13 @@
 window.onload = function () {
     alert("Valmis")
 }
+
+
+var massiiv = document.querySelectorAll('div.bead');
+
+for ( var i=0; i <massiiv.length; i++) {
+    massiiv[i].alt = "mingi pilt";
+
+}
+//alert(massiiv);
+alert(mass[i].length);
