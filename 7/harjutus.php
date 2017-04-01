f704652297881e4dacb751cdb6a2b450b2c4be0f
Indrek Pruul <ipruul@itcollege.ee>
2017-04-01 18:42:12 +0300

    php harjutus1

--- 7/harjutus.php
+++ 7/harjutus.php
@@ -1,8 +1,22 @@
-<?php echo "tere tali"
+<?php echo "tere tali";
+
 
 // see rida on kommentaar
 /* kõik siin vahel, isegi kui see on mitu rida pikk,
 on kommentaar */
 
+$muutuja =  "test";
+$muutuja2 = 123;
+echo " \n";
+echo "<br>";
+echo $muutuja;
+echo $muutuja2;
 
- <?= 4+4; ?>
+function loenda_lambaid($n){
+    $l = "lammas";
+    for($i=1; $i<$n; $i=$i+1) {
+        echo "$i.$l\";
+    }
+}
+loenda_lambaid(10);
+?>
